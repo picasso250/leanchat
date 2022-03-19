52d46621e10487bf3e391ed5079ec6d9fd9f4c8b
xiaochi <wxiaochi@qq.com>
2022-03-19 13:32:20 +0800

    history

--- js/joinroom.js
+++ js/joinroom.js
@@ -1,56 +1,56 @@
-$(document).ready(function(){
-	function salert(ti,txt,dur,type){
+$(document).ready(function () {
+	function salert(ti, txt, dur, type) {
 		Swal.fire({
-			title:ti,
-			icon:type,
-			text:txt,
-			timer:dur,
-			timerProgressBar:true
+			title: ti,
+			icon: type,
+			text: txt,
+			timer: dur,
+			timerProgressBar: true
 		});
 	}
-	if(!window.localStorage){
-		salert("错误","浏览器不支持LocalStorage",10000,"error");
+	if (!window.localStorage) {
+		salert("错误", "浏览器不支持LocalStorage", 10000, "error");
 		window.history.back();
 	}
-	var name=window.localStorage.getItem("name");
-	if(name=="TheGreatestMan"){
-		name=undefined;
+	var name = window.localStorage.getItem("name");
+	if (name == "TheGreatestMan") {
+		name = undefined;
 		window.localStorage.removeItem("name");
-		salert("错误","TheGreatestMan 为特殊权限账号",3000,"error");
-		window.location="../";
+		salert("错误", "TheGreatestMan 为特殊权限账号", 3000, "error");
+		window.location = "../";
 	}
-	if(name==undefined){
-		window.location="../";
+	if (name == undefined) {
+		window.location = "../";
 	}
 	var room;
-	function thenwedo(){
+	function thenwedo() {
 		$("#enterroom").remove();
-		const {Realtime,Event,TextMessage}=AV;
-		const realtime=new Realtime({
-			appId:"b39Oe3mhAGl6WwMndUNc8FT8-gzGzoHsz",
-			appKey:"RF7TuvH9B1756XJ8Xw0w6hOE",
-			server:"https://b39oe3mh.lc-cn-n1-shared.com"
+		const { Realtime, Event, TextMessage } = AV;
+		const realtime = new Realtime({
+			appId: "b39Oe3mhAGl6WwMndUNc8FT8-gzGzoHsz",
+			appKey: "RF7TuvH9B1756XJ8Xw0w6hOE",
+			server: "https://b39oe3mh.lc-cn-n1-shared.com"
 		});
-		realtime.createIMClient(name).then(function(me){
-			me.getConversation(room).then(function(conversation){
+		realtime.createIMClient(name).then(function (me) {
+			me.getConversation(room).then(function (conversation) {
 				return conversation.join();
-			}).then(function(conversation){
-				window.localStorage.setItem("roomid",room);
-				function getavatarurl(name){
+			}).then(function (conversation) {
+				window.localStorage.setItem("roomid", room);
+				function getavatarurl(name) {
 					$.ajax({
 						url: `https://api.github.com/users/${name}`,
-						success: function(ac){
-							$("#imagegoeshere").attr("src",ac.avatar_url);
-							$("#imagegoeshere").attr("id","")
+						success: function (ac) {
+							$("#imagegoeshere").attr("src", ac.avatar_url);
+							$("#imagegoeshere").attr("id", "")
 						},
-						error: function(x,e){
+						error: function (x, e) {
 							console.warn(e);
-							$("#imagegoeshere").attr("src",'https://avatars.githubusercontent.com/u/10137?v=4');
-							$("#imagegoeshere").attr("id","")
+							$("#imagegoeshere").attr("src", 'https://avatars.githubusercontent.com/u/10137?v=4');
+							$("#imagegoeshere").attr("id", "")
 						}
 					});
 				}
-				function addmsg(name,txt){
+				function addmsg(name, txt) {
 					$("#msgs").append(`<div class='item'>
 						<img class='ui avatar image' id='imagegoeshere'>
 						<div class='content' style="height:60px;width:95%">
@@ -58,13 +58,13 @@ $(document).ready(function(){
 						<div id="msggoeshere"></div>
 						</div></div>`);
 					$("#msggoeshere").text(txt);
-					$("#msggoeshere").attr("id","");
+					$("#msggoeshere").attr("id", "");
 					$("#namegoeshere").text(name);
-					$("#namegoeshere").attr("id","");
+					$("#namegoeshere").attr("id", "");
 					getavatarurl(name);
 				}
-				function addsys(txt){
-					var url="https://avatars.githubusercontent.com/u/66681986";
+				function addsys(txt) {
+					var url = "https://avatars.githubusercontent.com/u/66681986";
 					$("#msgs").append(`<div class='item'>
 						<img class='ui avatar image' src='${url}'>
 						<div class='content' style="height:60px;width:95%">
@@ -72,50 +72,60 @@ $(document).ready(function(){
 						<div id="msggoeshere"></div>
 						</div></div>`);
 					$("#msggoeshere").text(txt);
-					$("#msggoeshere").attr("id","");
+					$("#msggoeshere").attr("id", "");
 				}
 				$("#msgs").fadeIn(1000);
 				$("#msg").fadeIn(1000);
-				me.on(Event.MEMBERS_JOINED,function membersJoinedEventHandler(payload, conversation) {
+				me.on(Event.MEMBERS_JOINED, function membersJoinedEventHandler(payload, conversation) {
 					addsys(`${payload.members}加入了房间${conversation.name}[${conversation.id}]`);
 				});
-				me.on(Event.MESSAGE, function(message, conversation) {
-				    addmsg(message.from,message.text);
+				me.on(Event.MESSAGE, function (message, conversation) {
+					addmsg(message.from, message.text);
 				});
-				$("#send").click(function(){
-					var sending=$("#msgholder").val();
+				conversation.queryMessages({
+					limit: 10, // limit 取值范围 1~100，默认 20
+				}).then(function (messages) {
+					console.log(messages)
+					// 最新的十条消息，按时间增序排列
+					for(var i in messages){
+						var msg=messages[i]
+						addmsg(msg.from,msg.text)
+					}
+				}).catch(console.error.bind(console));
+				$("#send").click(function () {
+					var sending = $("#msgholder").val();
 					$("#msgholder").val("");
-					conversation.send(new TextMessage(sending)).then(function(message) {
-						addmsg(name,sending);
-						salert("成功！","已发送到云端！",500,"success")
-					}).catch(function(err){
-						salert("错误",`无法发送，详细信息：${err}`,10000,"error");
+					conversation.send(new TextMessage(sending)).then(function (message) {
+						addmsg(name, sending);
+						salert("成功！", "已发送到云端！", 500, "success")
+					}).catch(function (err) {
+						salert("错误", `无法发送，详细信息：${err}`, 10000, "error");
 					});
 				});
-				$("#exit").click(function(){
+				$("#exit").click(function () {
 					conversation.quit();
 					window.localStorage.removeItem("roomid");
-					salert("成功","已退出",2000,"success");
-					setTimeout(function(){
-						window.location=window.location;
-					},2000);
+					salert("成功", "已退出", 2000, "success");
+					setTimeout(function () {
+						window.location = window.location;
+					}, 2000);
 				});
-			}).catch(function(err){
-				salert("错误",`无法找到对应房间，详细信息：${err}`,10000,"error");
+			}).catch(function (err) {
+				salert("错误", `无法找到对应房间，详细信息：${err}`, 10000, "error");
 			});
-		}).catch(function(err){
-			salert("错误",`无法登录系统，详细信息：${err}`,10000,"error");
+		}).catch(function (err) {
+			salert("错误", `无法登录系统，详细信息：${err}`, 10000, "error");
 		});
 	}
-	if(window.localStorage.getItem("roomid")!=undefined){
-		room=window.localStorage.getItem("roomid");
+	if (window.localStorage.getItem("roomid") != undefined) {
+		room = window.localStorage.getItem("roomid");
 		thenwedo();
 	}
-	else{
+	else {
 		$("#enterroom").fadeIn(1000);
-		$("#setroom").click(function(){
-			room=$("#roomname").val();
-			$("#enterroom").fadeOut(1000,thenwedo());
+		$("#setroom").click(function () {
+			room = $("#roomname").val();
+			$("#enterroom").fadeOut(1000, thenwedo());
 		});
 	}
 });
--- js/nof12.js
+++ js/nof12.js
@@ -1,49 +1,4 @@
 document.onkeydown = function () {
 	if(window.location.host=="127.0.0.1:8848") //debug mode
 		return;
-	function salert(ti,txt,dur,type){
-		Swal.fire({
-			title:ti,
-			icon:type,
-			text:txt,
-			timer:dur,
-			timerProgressBar:true
-		});
-	}
-	var e = window.event || arguments[0];
-	if (e.keyCode == 123) {
-		salert("错误","操作被禁止",1000,"error");
-		return false;
-	}
-	else if((e.ctrlKey) && (e.shiftKey) && (e.keyCode == 73)) {
-		salert("错误","操作被禁止",1000,"error");
-		return false;
-	}
-	else if((e.ctrlKey)&&(e.keyCode == 85)) {
-		salert("错误","操作被禁止",1000,"error");
-		return false;
-	}
-	else if((e.shiftKey) && (e.keyCode == 121)) {
-		salert("错误","操作被禁止",1000,"error");
-		return false;
-	}
-	document.oncontextmenu = function(){
-		salert("错误","操作被禁止",1000,"error");
-		return false;
-	}
-}
-document.oncontextmenu = function () {
-	if(window.location.host=="127.0.0.1:8848")
-		return;
-	function salert(ti,txt,dur,type){
-		Swal.fire({
-			title:ti,
-			icon:type,
-			text:txt,
-			timer:dur,
-			timerProgressBar:true
-		});
-	}
-	salert("错误","操作被禁止",1000,"error");
-	return false;
 }
